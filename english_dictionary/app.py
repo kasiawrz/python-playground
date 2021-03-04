5817dfe04029fc36247310f0fa875d2ac5450bac
Kasia Wrzesniak <wrzesniak.k@gmail.com>
2021-03-05 00:51:01 +0100

    Use difflib to suggest another results if match not found

--- english_dictionary/app.py
+++ english_dictionary/app.py
@@ -1,13 +1,21 @@
 import json
+import difflib
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 
 def translate(word):
     word = word.lower()
+    dict_keys = data.keys()
 
     if word in data:
         return data[word]
+
+    elif get_close_matches(word, dict_keys):
+        return f"""Please double check your input. Did you mean: { get_close_matches(
+            word, dict_keys
+        )}?"""
     else:
         return "The word doesn't exist"
 
